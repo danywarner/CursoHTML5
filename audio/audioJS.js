f7f80bc215a7eacd14f1e6e1b85647318b1f98a2
Daniel Warner <danywarner@gmail.com>
2015-09-30 21:15:48 -0500

    audio

--- audio/audioJS.js
+++ audio/audioJS.js
@@ -1,10 +1,10 @@
-var actualvideo, play, barra, progreso,maximo;
+var myaudio, play, barra, progreso,maximo;
 
 maximo=600;
 
 function start(){
 
-	actualvideo=document.getElementById("actualvideo");
+	myaudio=document.getElementById("myaudio");
 	play=document.getElementById("play");
 	barra=document.getElementById("barra");
 	progreso=document.getElementById("progreso");
@@ -15,12 +15,12 @@ function start(){
 
 function clicking(){
 
-	if(actualvideo.paused===false && actualvideo.ended===false){
-		actualvideo.pause();
+	if(myaudio.paused===false && myaudio.ended===false){
+		myaudio.pause();
 		play.innerHTML="Play";
 	}
 	else{
-		actualvideo.play();
+		myaudio.play();
 		play.innerHTML="Pause";
 
 		bucle=setInterval(estado,1);
@@ -28,17 +28,17 @@ function clicking(){
 }
 
 function estado(){
-	if(actualvideo.ended===false){
-		var total=parseInt(actualvideo.currentTime*maximo/actualvideo.duration);
+	if(myaudio.ended===false){
+		var total=parseInt(myaudio.currentTime*maximo/myaudio.duration);
 		progreso.style.width=total+"px";
 	}
 }
 
 function adelantar(posicion){
-	if(actualvideo.paused===false && actualvideo.ended==false){
+	if(myaudio.paused===false && myaudio.ended==false){
 		var ratonX=posicion.pageX-barra.offsetLeft;
-		var nuevoTiempo=ratonX*actualvideo.duration/maximo;
-		actualvideo.currentTime=nuevoTiempo;
+		var nuevoTiempo=ratonX*myaudio.duration/maximo;
+		myaudio.currentTime=nuevoTiempo;
 		progreso.style.width=ratonX+"px";
 	}
 }
