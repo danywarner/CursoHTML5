c383452a494d410acb70221e6e24cea1c5b82153
Daniel Warner <danywarner@gmail.com>
2015-10-10 08:09:55 -0500

    drag & drop oculta origen

--- Dra&Drop/codigo.js
+++ Dra&Drop/codigo.js
@@ -1,12 +1,33 @@
 
+var elem_origen;
+var elem_destino;
+
 function start(){
-	var elem_origen=document.getElementById("imagen");
-	elem_origen.addEventListener("dragstart",function(){
-		alert("comenzó a moverse!");
-	},false);
+	elem_origen=document.getElementById("imagen");
+	elem_origen.addEventListener("dragstart",dragbegin,false);
+	elem_destino=document.getElementById("zonadestino");
+	elem_destino.addEventListener("dragenter",function(e){
+		e.preventDefault();},false);
+	elem_destino.addEventListener("dragover",function(e){
+		e.preventDefault();},false);
+	elem_destino.addEventListener("drop",dropped,false);
+	elem_origen.addEventListener("dragend", terminado, false);
 }
 
+function dragbegin(e){
+	var codigo="<img src='"+ elem_origen.getAttribute("src")+"'>";
+	e.dataTransfer.setData("Text", codigo);
+}
 
+function dropped(e){
+	e.preventDefault();
+	elem_destino.innerHTML=e.dataTransfer.getData("Text");
+}
+
+function terminado(e){
+	var elemento=e.target;
+	elemento.style.visibility="hidden"
+}
 
 
 
